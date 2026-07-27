7a2ab06afe343ed6cc653188049467234a60a06d
ua-voice <savetek.ua@gmail.com>
2026-07-27 13:31:08 +0300

    Fix Google Sheets sync timeout

--- callback-config.js
+++ callback-config.js
@@ -8,5 +8,5 @@
 window.CALLBACK_SERVICE = {
   endpoint: "https://script.google.com/macros/s/AKfycbxTFCk0hybHpyhbSAtd2tTc-lvt1qhygcpo5JGn7hFChg-xuAMj7V4oOzy3qrcBS4sP/exec",
   cooldownSeconds: 60,
-  configTimeoutMs: 3500
+  configTimeoutMs: 15000
 };
